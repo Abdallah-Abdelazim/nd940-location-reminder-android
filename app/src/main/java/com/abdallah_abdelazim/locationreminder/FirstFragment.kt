1c6032f48d269cd2c9400e5344c36d4ee95025e4
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-03-04 01:45:52 +0200

    Reformat code & optimize imports

--- app/src/main/java/com/abdallah_abdelazim/locationreminder/FirstFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/locationreminder/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.abdallah_abdelazim.locationreminder
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.abdallah_abdelazim.locationreminder.databinding.FragmentFirstBinding
 
--- app/src/main/java/com/abdallah_abdelazim/locationreminder/MainActivity.kt
+++ app/src/main/java/com/abdallah_abdelazim/locationreminder/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.abdallah_abdelazim.locationreminder
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.abdallah_abdelazim.locationreminder.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
