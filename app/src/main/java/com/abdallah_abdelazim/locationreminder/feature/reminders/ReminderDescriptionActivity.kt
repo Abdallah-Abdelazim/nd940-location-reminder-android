cd035d1c15ad0819bdabb399ed7c068aec46cafc
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-03-06 23:08:27 +0200

    Finish `ReminderDescriptionActivity`

--- app/src/main/java/com/abdallah_abdelazim/locationreminder/feature/reminders/ReminderDescriptionActivity.kt
+++ app/src/main/java/com/abdallah_abdelazim/locationreminder/feature/reminders/ReminderDescriptionActivity.kt
@@ -2,6 +2,7 @@ package com.abdallah_abdelazim.locationreminder.feature.reminders
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -10,28 +11,32 @@ import com.abdallah_abdelazim.locationreminder.databinding.ActivityReminderDescr
 import com.abdallah_abdelazim.locationreminder.feature.reminders.reminderslist.ReminderUiModel
 
 /**
- * Activity that displays the reminder details after the user clicks on the notification
+ * Activity that displays the reminder details after the user clicks on the notification.
  */
 class ReminderDescriptionActivity : AppCompatActivity() {
-
-    companion object {
-        private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
-
-        //        receive the reminder object after the user clicks on the notification
-        fun newIntent(context: Context, reminderUiModel: ReminderUiModel): Intent {
-            val intent = Intent(context, ReminderDescriptionActivity::class.java)
-            intent.putExtra(EXTRA_ReminderDataItem, reminderUiModel)
-            return intent
-        }
-    }
 
     private lateinit var binding: ActivityReminderDescriptionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_reminder_description
-        )
-//        TODO: Add the implementation of the reminder details
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_reminder_description)
+
+        val reminder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getSerializableExtra(EXTRA_REMINDER_DATA_ITEM, ReminderUiModel::class.java)
+        } else {
+            intent.getSerializableExtra(EXTRA_REMINDER_DATA_ITEM) as ReminderUiModel
+        }
+        binding.reminder = reminder
+    }
+
+    companion object {
+
+        private const val EXTRA_REMINDER_DATA_ITEM = "EXTRA_ReminderDataItem"
+
+        fun newIntent(context: Context, reminderUiModel: ReminderUiModel): Intent {
+            val intent = Intent(context, ReminderDescriptionActivity::class.java)
+            intent.putExtra(EXTRA_REMINDER_DATA_ITEM, reminderUiModel)
+            return intent
+        }
     }
 }
